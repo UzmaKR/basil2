4ab071cbcaa10354e69a226963dba006b7cdde57
Uzma Rahman <uzma.rahman@gmail.com>
2013-03-16 14:26:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basil2::Application.config.secret_token = '7b324ddddf985bde4e365616831923f958d7995c56d2a5bdc816e3c4ea56d92120424e7e717c3112c381dff10323437c2ddc3edd2f5f8cfd127bb581fd268ab1'
